51dbddf187c42ffa44a5ce6cc0c9f2cf77bbd2fb
LTMC4 <LTMC4@10.0.0.21>
2020-04-23 12:49:06 -0500

    Fixed program, Working on Admin

--- containersoftware/src/main/java/containersoftware/FirstPage.java
+++ containersoftware/src/main/java/containersoftware/FirstPage.java
@@ -9,6 +9,7 @@ import javax.swing.SwingConstants;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
 
 public class FirstPage {
 
